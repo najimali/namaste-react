cbe3752bb5927145bec46a4c14dfb86788a7ab28
najimali <mohd.najim@zyod.com>
2023-12-10 14:56:12 +0530

    improve shimmer styles

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,22 +4,23 @@ import "../styles/body.css"
 import { useEffect, useState } from "react";
 import useFetch from "../hooks/useFetch";
 import { SWIGGY_RESTAURANT_API_END_POINT } from "../utils/constant";
-import Shimmer from "./Shimmer";
-const shimmerArray = new Array(9).fill(null);
+import RestaurantCardShimmer from "./ResturantCardShimmer"
+const restaurantCardShimmerArray = new Array(16).fill(null);
 const Body = () => {
     const { data: restaurants, loading } = useFetch(SWIGGY_RESTAURANT_API_END_POINT)
 
     if (loading) {
-        return (<div className="restaurant-container">
-            {shimmerArray.map((_, index) => <Shimmer key={index} />)}
-        </div>
+        return (
+            <div className="restaurant-container">
+                {restaurantCardShimmerArray.map((_, index) => <RestaurantCardShimmer key={index} />)}
+            </div>
         )
     }
     return (
         <div className="body">
             <SearchBar />
             <div className="restaurant-container">
-                {restaurants.map(({ info }) => (<RestaurantCard restaurant={info} />))}
+                {restaurants?.map(({ info }) => (<RestaurantCard restaurant={info} />))}
             </div>
         </div>
 
--- src/components/ResturantCardShimmer.js
+++ src/components/ResturantCardShimmer.js
@@ -1,11 +1,11 @@
-import "../styles/shimmer.css"
-const Shimmer = () => {
+import "../styles/restaurant-card-shimmer.css"
+const RestaurantCardShimmer = () => {
     return (
-        <div className="simmer-card">
+        <div className="shimmer-card">
             <div className="shimmer-loader-2"></div>
             <div className="shimmer-loader-1"></div>
             <div className="shimmer-loader-1"></div>
         </div>
     );
 }
-export default Shimmer
+export default RestaurantCardShimmer
